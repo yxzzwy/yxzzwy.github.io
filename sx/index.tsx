fcf96695f0f930bf9c36d38d25497071031b9cca
yxzzwy <98509246+yxzzwy@users.noreply.github.com>
2022-06-08 22:07:51 +0800

    Delete sx directory

--- sx/index.tsx
+++
@@ -1,7 +0,0 @@
-import 'react-hot-loader';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './src/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
